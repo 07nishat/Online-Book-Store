004b5214d7f9479a382c5596f15ee68385e71556
Nishat Salsabin <nishatsalsabin524@gmail.com>
2024-05-24 21:41:49 +0600

    first commit

---
+++ about.php
@@ -0,0 +1,178 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>About</title>
+
+   <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css" />
+   
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'components/user_header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/23.PNG" alt="">
+      </div>
+
+      <div class="content">
+         <h3>Developer's Message:</h3>
+         <p>Hello! I'm Nishat Salsabin. Graduated from East West University, department of CSE (2023) . I love designing websites and exploring new things.</p>
+
+         <p>I would like to thank Mahmudul Hasan Munna</a> Sir for guiding me through the university course and making me able to develop projects like this. </p>
+         <a href="contact.php" class="btn">Contact Us</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+   
+   <h1 class="heading">Client's Reviews.</h1>
+
+   <div class="swiper reviews-slider">
+
+   <div class="swiper-wrapper">
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-5.JPG" alt="">
+         <p>Been using their services for quite a bit and have never had an issue with the quality of their products.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3> <a href="https://www.facebook.com/nigarsultana1122" target="_blank">Nigar Sultana</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-1.JPG" alt="">
+         <p>I always unbox making a video and instantly complain if there's anything wrong. They are very fast to solve any customer issues</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/profile.php?id=61557836649309" target="_blank">Nabila Anjum</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-3.JPG" alt="">
+         <p>Proshadhoni provide the authentic beauty produts . I always Buy product from Proshadhoni.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/sadiaafrin.bristi.902" target="_blank">Sadia Afrin</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-7.JPG" alt="">
+         <p>Using Proshadhoni for online shopping from almost 3 years. Outstanding experience with them.  </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/tunazzina.mou" target="_blank">Tunazzina Mou</a></h3>
+      </div>
+
+      <div class="swiper-slide slide">
+         <img src="images/pic-2.JPG" alt="">
+         <p>I have been using their services for the last 2 years and I have found them extremely reliable.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3><a href="https://www.facebook.com/tahsin4199" target="_blank">Anika Tahsin</a></h3>
+      </div>
+
+      
+
+   </div>
+
+   <div class="swiper-pagination"></div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>
+
+<script src="https://unpkg.com/swiper@8/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+<script>
+
+var swiper = new Swiper(".reviews-slider", {
+   loop:true,
+   spaceBetween: 20,
+   pagination: {
+      el: ".swiper-pagination",
+      clickable:true,
+   },
+   breakpoints: {
+      0: {
+        slidesPerView:1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      991: {
+        slidesPerView: 3,
+      },
+   },
+});
+
+</script>
+
+</body>
+</html>
